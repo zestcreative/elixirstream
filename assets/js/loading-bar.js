6249479dc86c59c84107b49f63d46f7b084e4d12
David Bernheisel <david@bernheisel.com>
2020-08-09 18:53:18 -0400

    Fix loading bar delay

--- assets/js/loading-bar.js
+++ assets/js/loading-bar.js
@@ -1,10 +1,18 @@
 import topbar from "topbar";
+let topbarDelay = null;
 
+// Show progress bar on live navigation and form submits
 topbar.config({
   barThickness: 2,
   shadowBlur: 5,
   barColors: ["#F56565", "#9B2C2C"],
 })
 
-window.addEventListener("phx:page-loading-start", _info => topbar.show());
-window.addEventListener("phx:page-loading-stop", _info => topbar.hide());
+window.addEventListener("phx:page-loading-start", _info => {
+  clearTimeout(topbarDelay);
+  topbarDelay = setTimeout(() => topbar.show(), 200);
+})
+window.addEventListener("phx:page-loading-stop", _info => {
+  clearTimeout(topbarDelay);
+  topbar.hide();
+})
