96fa8019a784200e35440a48f5e3f0b7ace072ea
David Bernheisel <david@bernheisel.com>
2020-10-24 23:24:59 -0400

    Add telemetry for HTTP sink

--- lib/utility_web/controllers/sink_controller.ex
+++ lib/utility_web/controllers/sink_controller.ex
@@ -5,7 +5,13 @@ defmodule UtilityWeb.SinkController do
   action_fallback ErrorController
 
   def any(conn, %{"foo_sink_id" => id}) when byte_size(id) == 36 do
+    start = System.monotonic_time()
     HttpSink.broadcast(id, HttpSink.build(conn))
+    :telemetry.execute(
+      [:utility, :sink, :build],
+      %{duration: System.monotonic_time() - start},
+      conn
+    )
 
     conn
     |> put_status(200)
--- lib/utility_web/telemetry.ex
+++ lib/utility_web/telemetry.ex
@@ -32,7 +32,11 @@ defmodule UtilityWeb.Telemetry do
       summary("vm.memory.total", unit: {:byte, :kilobyte}),
       summary("vm.total_run_queue_lengths.total"),
       summary("vm.total_run_queue_lengths.cpu"),
-      summary("vm.total_run_queue_lengths.io")
+      summary("vm.total_run_queue_lengths.io"),
+
+      # Utility
+      counter("utility.sink.build.duration"),
+      summary("utility.sink.build.duration", unit: {:native, :millisecond})
     ]
   end
 
