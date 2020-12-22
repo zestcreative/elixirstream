a02ebad3b46b3187f7087588f3f94b8d27167712
David Bernheisel <david@bernheisel.com>
2020-12-22 15:19:40 -0500

    Fix hidden input

--- lib/utility_web/controllers/sink_controller.ex
+++ lib/utility_web/controllers/sink_controller.ex
@@ -2,11 +2,10 @@ defmodule UtilityWeb.SinkController do
   use UtilityWeb, :controller
   alias UtilityWeb.{HttpSink, SinkLive}
 
-  action_fallback ErrorController
-
   def any(conn, %{"foo_sink_id" => id}) when byte_size(id) == 36 do
     start = System.monotonic_time()
     HttpSink.broadcast(id, HttpSink.build(conn))
+
     :telemetry.execute(
       [:utility, :sink, :build],
       %{duration: System.monotonic_time() - start},
@@ -16,5 +15,12 @@ defmodule UtilityWeb.SinkController do
     conn
     |> put_status(200)
     |> text("See sink at #{Routes.live_url(conn, SinkLive, id)}")
+  end
+
+  def any(conn, _params) do
+    conn
+    |> put_status(400)
+    |> put_view(UtilityWeb.ErrorView)
+    |> render("400.html")
   end
 end
