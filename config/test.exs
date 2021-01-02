8d084c36d9f6c057887c0212d2cdaaf8002fe82e
David Bernheisel <david@bernheisel.com>
2021-01-01 20:10:27 -0500

    Fix CI

--- config/test.exs
+++ config/test.exs
@@ -2,10 +2,16 @@ import Config
 
 config :utility, cache: Utility.Test.MockCache
 
-config :utility, Utility.Repo,
-  database: "utility_test#{System.get_env("MIX_TEST_PARTITION")}",
-  hostname: "localhost",
-  pool: Ecto.Adapters.SQL.Sandbox
+if System.get_env("CI") do
+  config :utility, Utility.Repo,
+    url: "postgres://postgres:postgres@postgres:5432/utility_test#{System.get_env("MIX_TEST_PARTITION")}",
+    pool: Ecto.Adapters.SQL.Sandbox
+else
+  config :utility, Utility.Repo,
+    database: "utility_test#{System.get_env("MIX_TEST_PARTITION")}",
+    hostname: "localhost",
+    pool: Ecto.Adapters.SQL.Sandbox
+end
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
