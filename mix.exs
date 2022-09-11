cc984991ec7070c0e38af88e3e3f5b42cffc7f6c
David Bernheisel <david@bernheisel.com>
2022-09-11 16:52:29 -0400

    Fix theme chooser resetting

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Utility.MixProject do
       version: String.trim(File.read!("VERSION")),
       elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [:gettext] ++ Mix.compilers(),
+      compilers: Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps(),
