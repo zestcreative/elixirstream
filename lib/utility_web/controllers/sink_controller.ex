077f8d7d9b781e6e9c3246f0a6e64ee347c95dca
David Bernheisel <david@bernheisel.com>
2023-01-04 14:43:47 -0500

    Use full URL in sink response

--- lib/utility_web/controllers/sink_controller.ex
+++ lib/utility_web/controllers/sink_controller.ex
@@ -14,7 +14,7 @@ defmodule UtilityWeb.SinkController do
 
     conn
     |> put_status(200)
-    |> text("See sink at #{~p"/sink/view/#{id}"}\n")
+    |> text("See sink at #{url(~p"/sink/view/#{id}")}\n")
   end
 
   def any(conn, _params) do
