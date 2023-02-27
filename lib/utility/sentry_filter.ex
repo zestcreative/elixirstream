09a43795d3511e3009a299d9198f18080deeb7e4
David Bernheisel <david@bernheisel.com>
2023-02-27 00:40:51 -0500

    Hopefully this gets the error

--- lib/utility/sentry_filter.ex
+++ lib/utility/sentry_filter.ex
@@ -1,6 +1,6 @@
 defmodule Utility.SentryFilter do
   @behaviour Sentry.EventFilter
 
-  def exclude_exception?(%Elixir.Phoenix.Router.NoRouteError{}, :plug), do: true
+  def exclude_exception?(%Phoenix.Router.NoRouteError{}, :logger), do: true
   def exclude_exception?(_exception, _source), do: false
 end
