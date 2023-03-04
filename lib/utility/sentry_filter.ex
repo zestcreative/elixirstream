46f09e0ea6b84bcb80bd52f0bccd5e0e9e35c94a
David Bernheisel <david@bernheisel.com>
2023-03-03 20:32:16 -0500

    Get that stupid exception out of here

--- lib/utility/sentry_filter.ex
+++ lib/utility/sentry_filter.ex
@@ -1,6 +1,6 @@
 defmodule Utility.SentryFilter do
   @behaviour Sentry.EventFilter
 
-  def exclude_exception?(%Phoenix.Router.NoRouteError{}, :logger), do: true
+  def exclude_exception?(%Phoenix.Router.NoRouteError{}, _), do: true
   def exclude_exception?(_exception, _source), do: false
 end
